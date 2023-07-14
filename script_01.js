34ad6af176f59d302b8622cc1794d43f252d5b8a
Trandoner <trand_oner@gmx.de>
2023-07-14 11:39:15 +0200

    IF - ELSE

--- script_01.js
+++ script_01.js
@@ -19,3 +19,42 @@ console.log("ageMark: " + ageMark);
 console.log("isJohnOlder: " + isJohnOlder);
 console.log("isJohnEqual: " + isJohnEqual);
 console.log("------------------");
+
+/************ IF ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+// if(true)  //fake it, ´till you make it!
+// if(isJohnOlder)
+// if(ageJohn > ageMark)
+// {
+//     console.log("John ist älter");  //control flow
+// }
+
+/************ IF - ELSE ************/
+
+// mit Alternative
+// entweder ja oder nein
+
+// if (true)
+// if (false)
+// 1. TEST
+if(isJohnOlder)
+{
+    console.log("John ist älter");  
+} 
+// 1...n Alternative
+else if(isJohnEqual)
+{
+    console.log("John ist gleich alt ");  
+}
+// wenn alle vorherigen Alternativen fehlschlagen !
+else
+{
+    console.log("john ist jünger")
+}
+
+/************ Ternäre (ternary) Schreibweise ************/
+
+// console.log( (isJohnOlder) ? "John is older" : "Mark is older");
